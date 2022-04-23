ba6c8de1de6208c356f645b0288802e251be8f6e
NamH <NamNh0122@gmail.com>
2022-04-24 00:19:44 +0700

    feat(SYRFDeviceInfo): adding device info module
    Signed-off-by: NamH <NamNh0122@gmail.com>

---
+++ SYRFDeviceInfo/SYRFDeviceInfo/SYRFDeviceInfo.h
@@ -0,0 +1,18 @@
+//
+//  SYRFDeviceInfo.h
+//  SYRFDeviceInfo
+//
+//  Created by James on 23/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SYRFDeviceInfo.
+FOUNDATION_EXPORT double SYRFDeviceInfoVersionNumber;
+
+//! Project version string for SYRFDeviceInfo.
+FOUNDATION_EXPORT const unsigned char SYRFDeviceInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SYRFDeviceInfo/PublicHeader.h>
+
+
